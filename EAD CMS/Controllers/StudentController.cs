a4cf261e27f44db393a0899d22c1aaedb8e43d67
BITF14M034 <bitf14m034@pucit.edu.pk>
2017-07-11 06:45:11 +0500

    Term Project 1

---
+++ EAD CMS/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using EAD_CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EAD_CMS.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        public ActionResult Student()
+        {
+            student stu = new student();
+
+            string username = (string)Session["username"];
+
+            using (CMSEntities db=new CMSEntities())
+            {
+                var userDetails = db.students.Where(x => x.rollno == username).FirstOrDefault();
+
+                if (userDetails != null)
+                {
+                    stu.name = userDetails.name.ToUpper();
+                    stu.rollno = userDetails.rollno;
+                    stu.degree = userDetails.degree;
+                    stu.batch = userDetails.batch;
+                }
+                
+            }
+
+            return View(stu);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+	}
+}
---
+++ EAD CMS/Controllers/TeacherController.cs
@@ -0,0 +1,36 @@
+﻿using EAD_CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EAD_CMS.Controllers
+{
+    public class TeacherController : Controller
+    {
+        //
+        // GET: /Teacher/
+        public ActionResult Teacher()
+        {
+            Teacher teacher = new Teacher();
+            
+            string username = (string)Session["username"];
+
+            using (CMSEntities db = new CMSEntities())
+            {
+                var userDetails = db.Teachers.Where(x => x.username == username).FirstOrDefault();
+
+                teacher.name = userDetails.name.ToUpper();
+                teacher.username = userDetails.username;
+
+            }
+
+            return View(teacher);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+	}
+}
